43d60d4a54eaba6c66b4a396a76469b3a103516c
Warren Falk <wfalk@divisionsinc.com>
2013-07-19 14:45:18 -0400

    fix formatting of GameRenderer.java

--- src/com/warrenfalk/mercury/GameRenderer.java
+++ src/com/warrenfalk/mercury/GameRenderer.java
@@ -32,55 +32,55 @@ public class GameRenderer implements GLSurfaceView.Renderer {
 
 	float width, height;
 
-    public GameRenderer() {
-    }
+	public GameRenderer() {
+	}
 
-    @Override
-    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 
-    }
+	}
 
-    @Override
-    public void onSurfaceChanged(GL10 gl, int w, int h) {
-	width = w;
-	height = h;
-	gl.glViewport(0, 0, w, h);
+	@Override
+	public void onSurfaceChanged(GL10 gl, int w, int h) {
+		width = w;
+		height = h;
+		gl.glViewport(0, 0, w, h);
 
-	gl.glMatrixMode(GL10.GL_PROJECTION);
-	gl.glLoadIdentity();
+		gl.glMatrixMode(GL10.GL_PROJECTION);
+		gl.glLoadIdentity();
 
-	// orthographic
-	float ratio = width / height;
-	Vec2 upperLeft = new Vec2(0f, 0f);
-	Vec2 lowerRight = new Vec2(width, height);
-	gl.glOrthof(upperLeft.x, lowerRight.x, lowerRight.y, upperLeft.y, -10.0f, 10.0f);
-	//gl.glFrustumf(-10.0f, 10.0f, 10.0f, -10.0f, 0.1f, 100.0f);
-	gl.glViewport(0, 0, (int) width, (int) height);
-	gl.glMatrixMode(GL10.GL_MODELVIEW);
+		// orthographic
+		float ratio = width / height;
+		Vec2 upperLeft = new Vec2(0f, 0f);
+		Vec2 lowerRight = new Vec2(width, height);
+		gl.glOrthof(upperLeft.x, lowerRight.x, lowerRight.y, upperLeft.y, -10.0f, 10.0f);
+		// gl.glFrustumf(-10.0f, 10.0f, 10.0f, -10.0f, 0.1f, 100.0f);
+		gl.glViewport(0, 0, (int) width, (int) height);
+		gl.glMatrixMode(GL10.GL_MODELVIEW);
 
-	gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-	gl.glClearColor(0f, 0f, 0f, 1.0f);
-    }
+		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+		gl.glClearColor(0f, 0f, 0f, 1.0f);
+	}
 
-    @Override
-    public void onDrawFrame(GL10 gl) {
-	gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-	gl.glLoadIdentity();
+	@Override
+	public void onDrawFrame(GL10 gl) {
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		gl.glLoadIdentity();
 
-	LinkedList<Renderable> add = null;
-	if (toAdd != null) {
-		synchronized (renderables) {
-			add = toAdd;
-			toAdd = null;
+		LinkedList<Renderable> add = null;
+		if (toAdd != null) {
+			synchronized (renderables) {
+				add = toAdd;
+				toAdd = null;
+			}
+		}
+		if (add != null)
+			for (Renderable renderable : add)
+				renderables.push(renderable);
+		for (Renderable renderable : renderables) {
+			renderable.prerender(gl);
+			renderable.render(gl);
+			renderable.postrender(gl);
 		}
 	}
-	if (add != null)
-		for (Renderable renderable : add)
-			renderables.push(renderable);
-	for (Renderable renderable : renderables) {
-		renderable.prerender(gl);
-		renderable.render(gl);
-		renderable.postrender(gl);
-	}
-    }
 }
