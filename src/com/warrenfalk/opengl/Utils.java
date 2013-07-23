1fedff3d8b8aa68f9c56d679bc5c8f9c87c30d89
Warren Falk <wfalk@divisionsinc.com>
2013-07-23 15:45:59 -0400

    make texture load be edge-clamped and non-tiled by default

--- src/com/warrenfalk/opengl/Utils.java
+++ src/com/warrenfalk/opengl/Utils.java
@@ -54,8 +54,8 @@ public class Utils {
 		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
 		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
 
-		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_S, GL10.GL_REPEAT);
-		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_T, GL10.GL_REPEAT);
+		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_S, GL10.GL_CLAMP_TO_EDGE);
+		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_T, GL10.GL_CLAMP_TO_EDGE);
 
 		// Use the Android GLUtils to specify a two-dimensional texture image
 		// from our bitmap
